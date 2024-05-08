54fdeb10e266491253078018dadb5b832f980cdd
MarquesDePistacho <artem.azhdaryan@yandex.ru>
2024-05-09 00:58:33 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # config.py
 
-TOKEN = "7027242117:AAEGWvLY22_ewHc9WCzFS6TRbxt4hCYjtHQ"
-# TOKEN = input("Введите TOKEN: ")
-GOOGLE_API_KEY = 'AIzaSyAXB4KvIrrilZ_6i8L5T1FkcSjxaVENI9o'
-# GOOGLE_API_KEY = input("Введите GOOGLE_API_KEY: ")
+
+TOKEN = input("Введите TOKEN: ")
+
+GOOGLE_API_KEY = input("Введите GOOGLE_API_KEY: ")
